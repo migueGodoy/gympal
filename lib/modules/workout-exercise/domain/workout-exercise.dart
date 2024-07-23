7c64b5646809ae4a2aa5b8495eabf6c19e0297b9
Migue Godoy <mgodoy@power-electronics.com>
2024-07-23 12:40:11 +0200

    refactor: Add id field to WorkoutExercise class

--- lib/modules/workout-exercise/domain/workout-exercise.dart
+++ lib/modules/workout-exercise/domain/workout-exercise.dart
@@ -2,16 +2,19 @@ import '../../exercise/domain/exercise.dart';
 import '../../series/domain/series.dart';
 
 class WorkoutExercise {
+  final String id;
   final Exercise exercise;
   final List<Series> series;
 
   WorkoutExercise({
+    required this.id,
     required this.exercise,
     required this.series,
   });
 
   Map<String, dynamic> toMap() {
     return {
+      'id': id,
       'exercise': exercise.toMap(),
       'series': series.map((s) => s.toMap()).toList(),
     };
@@ -19,6 +22,7 @@ class WorkoutExercise {
 
   factory WorkoutExercise.fromMap(Map<String, dynamic> map) {
     return WorkoutExercise(
+      id: map['id'],
       exercise: Exercise.fromMap(map['exercise']),
       series: List<Series>.from(map['series'].map((s) => Series.fromMap(s))),
     );
