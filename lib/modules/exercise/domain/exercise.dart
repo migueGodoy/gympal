30b3d49e7403f352b6028879f1b40ffe5c3e2dc9
Migue Godoy <mgodoy@power-electronics.com>
2024-07-23 15:47:51 +0200

    refactor: Add validation for id and name in Exercise constructor

--- lib/modules/exercise/domain/exercise.dart
+++ lib/modules/exercise/domain/exercise.dart
@@ -6,12 +6,15 @@ class Exercise {
   final String description;
   final Category category;
 
-  Exercise({
-    required this.id,
-    required this.name,
-    required this.description,
-    required this.category,
-  });
+  Exercise({required this.id, required this.name, required this.description, required this.category}) {
+    if (id.isEmpty) {
+      throw ArgumentError('Id cannot be empty');
+    }
+
+    if (name.isEmpty) {
+      throw ArgumentError('Name cannot be empty');
+    }
+  }
 
   Map<String, dynamic> toMap() {
     return {
